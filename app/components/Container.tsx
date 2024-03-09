ff576767b6c0aeceb73ccea5c5d0864584beddb9
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-03-09 16:38:18 -0300

    Add classes as props to container

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface ContainerProps {
   children: React.ReactNode;
+  classes?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container: React.FC<ContainerProps> = ({ children, classes = '' }) => {
   return (
-    <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
+    <div className={`max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 className ${classes}`}>
       {children}
     </div>
   );
